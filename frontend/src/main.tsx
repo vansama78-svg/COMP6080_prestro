500cbd3ae4a31fa613504fb3c290af77476dfdcf
Songning Liu <z5668661@unsw.edu.au>
2026-04-15 01:20:41 +1000

    M2.1 Set up app entry and protected route structure

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import { AuthProvider } from "./context/AuthProvider.tsx";
+import { ErrorProvider } from "./context/ErrorProvider.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ErrorProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
+    </ErrorProvider>
   </StrictMode>,
-)
+);
