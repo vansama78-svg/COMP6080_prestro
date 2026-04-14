5a32e155d84a8054d766e15948d1e1939fa3ce77
Songning Liu <z5668661@unsw.edu.au>
2026-04-15 03:03:59 +1000

    Edit config.ts

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
 import backendConfig from "../backend.config.json";
 
-/** Base URL for the Presto backend (see `frontend/backend.config.json`). */
+/** Base URL for the Presto backend. */
 export const API_BASE_URL = `http://127.0.0.1:${String(backendConfig.BACKEND_PORT)}`;
