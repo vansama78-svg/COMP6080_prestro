5c426fe32917e27323bb9f804f238fb24433bcf0
Songning Liu <z5668661@unsw.edu.au>
2026-04-18 23:35:06 +1000

    M2.3: Add image, video, and code elements with modals and store persistence

--- frontend/src/types/presentation.ts
+++ frontend/src/types/presentation.ts
@@ -1,7 +1,58 @@
 export type Slide = {
   id: string;
-  elements: unknown[];
+  elements: SlideElement[];
 };
+
+export type TextElement = {
+  id: string;
+  type: "text";
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  content: string;
+  fontSizeEm: number;
+  color: string;
+  layer: number;
+};
+
+export type ImageElement = {
+  id: string;
+  type: "image";
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  src: string;
+  alt: string;
+  layer: number;
+};
+
+export type VideoElement = {
+  id: string;
+  type: "video";
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  url: string;
+  autoplay: boolean;
+  layer: number;
+};
+
+export type CodeElement = {
+  id: string;
+  type: "code";
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  code: string;
+  fontSizeEm: number;
+  layer: number;
+};
+
+export type SlideElement = TextElement | ImageElement | VideoElement | CodeElement;
 
 export type Presentation = {
   id: string;
