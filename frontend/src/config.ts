3d92b8c9149b577eaf73634973a0a7e15bf51727
Songning Liu <z5668661@ad.unsw.edu.au>
2026-04-20 05:18:59 +1000

    Connect to deployed backend

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,2 @@
-import backendConfig from "../backend.config.json";
-
 /** Base URL for the Presto backend (see `frontend/backend.config.json`). */
 export const API_BASE_URL = `https://comp-6080-prestro-9wga-3ubncs1m0-vansama78-svgs-projects.vercel.app/`;
